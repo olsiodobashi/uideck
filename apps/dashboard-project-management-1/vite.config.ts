6d571fd3c4d24bc7eba70bc055e8ae0740d2ac82
olsiodobashi <olsiodobashi@gmail.com>
2025-06-04 22:32:44 -0400

    update: add github pages action

--- apps/dashboard-project-management-1/vite.config.ts
+++ apps/dashboard-project-management-1/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => ({
+  name: "codepen-dashboard-1",
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/dashboard-project-management-1',
   server: {
