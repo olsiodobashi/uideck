62200acdec03b222ca1c1da6de13d0a4f122ac8c
olsiodobashi <olsiodobashi@gmail.com>
2025-06-04 22:34:33 -0400

    fix: utils linter errors

--- apps/dashboard-project-management-1/src/utils.ts
+++ apps/dashboard-project-management-1/src/utils.ts
@@ -1,13 +1,13 @@
 /**
  * Convert HEX to RGB
  */
-export const convertHex = (hex, opacity) => {
-   hex = hex.replace("#", "");
-   var r = parseInt(hex.substring(0, 2), 16);
-   var g = parseInt(hex.substring(2, 4), 16);
-   var b = parseInt(hex.substring(4, 6), 16);
+export const convertHex = (hex: string, opacity: number) => {
+  hex = hex.replace('#', '');
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
 
-   return "rgba(" + r + "," + g + "," + b + "," + opacity / 100 + ")";
+  return 'rgba(' + r + ',' + g + ',' + b + ',' + opacity / 100 + ')';
 };
 
 export const setStatusClass = (status: string) => {
